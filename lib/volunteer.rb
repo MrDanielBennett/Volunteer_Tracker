4df65a79f0d2909f26b6a1b40e31476f66bbe23a
MrDanielBennett <daniel.bennett1999@gmail.com>
2018-12-07 11:43:21 -0800

    add save function to volunteer now test passes (13 tests pass)

--- lib/volunteer.rb
+++ lib/volunteer.rb
@@ -6,4 +6,15 @@ class Volunteer
     @project_id = attributes.fetch(:project_id)
     @id = attributes.fetch(:id)
   end
+
+  def ==(another_volunteer)
+    self.name().==(another_volunteer.name()).&self.project_id().==(another_volunteer.project_id)
+  end
+
+  def save
+    result = DB.exec("INSERT INTO volunteers(name, project_id) VALUES ('#{@title}', '#{@project_id}') RETURNING id;")
+    @name = result.first.fetch("name")
+    @project_id = result.first.fetch("project_id")
+    @id = result.first().fetch("id").to_i()
+  end
 end
