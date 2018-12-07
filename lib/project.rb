d3b8691d0ed4791ddbed3b62089d42faa99c9883
MrDanielBennett <daniel.bennett1999@gmail.com>
2018-12-07 12:51:26 -0800

    add volunteers method to project.rb and now every test passes (18 tests pass)

--- lib/project.rb
+++ lib/project.rb
@@ -41,4 +41,16 @@ class Project
     end
     projects
   end
+
+  def volunteers
+    returned_volunteers = DB.exec ("SELECT * FROM volunteers WHERE project_id = #{id}")
+    volunteers = []
+    returned_volunteers.each do |volunteer|
+      name = volunteer.fetch("name")
+      project_id = volunteer.fetch("project_id").to_i()
+      id = volunteer.fetch("id").to_i()
+      volunteers.push(Volunteer.new({:name => name, :project_id => project_id, :id => id}))
+  end
+  volunteers
+end
 end
