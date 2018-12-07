882d597fdbee5a79921e81bc2ebcdb82371cc5c4
MrDanielBennett <daniel.bennett1999@gmail.com>
2018-12-07 11:29:49 -0800

    create volunteer class, now basic specs for volunteer work (12 tests pass)

---
+++ lib/volunteer.rb
@@ -0,0 +1,9 @@
+class Volunteer
+  attr_reader :name, :project_id, :id
+
+  def initialize(attributes)
+    @name = attributes.fetch(:name)
+    @project_id = attributes.fetch(:project_id)
+    @id = attributes.fetch(:id)
+  end
+end
--- spec/volunteer_spec.rb
+++ spec/volunteer_spec.rb
@@ -1,20 +1,20 @@
 require "spec_helper"
 
-# describe Volunteer do
-#   describe '#name' do
-#     it 'returns the name of the volunteer' do
-#       test_volunteer = Volunteer.new({:name => 'Jane', :project_id => 1, :id => nil})
-#       expect(test_volunteer.name).to eq 'Jane'
-#     end
-#   end
-#
-#   describe '#project_id' do
-#     it 'returns the project_id of the volunteer' do
-#       test_volunteer = Volunteer.new({:name => 'Jane', :project_id => 1, :id => nil})
-#       expect(test_volunteer.project_id).to eq 1
-#     end
-#   end
-#
+describe Volunteer do
+  describe '#name' do
+    it 'returns the name of the volunteer' do
+      test_volunteer = Volunteer.new({:name => 'Jane', :project_id => 1, :id => nil})
+      expect(test_volunteer.name).to eq 'Jane'
+    end
+  end
+
+  describe '#project_id' do
+    it 'returns the project_id of the volunteer' do
+      test_volunteer = Volunteer.new({:name => 'Jane', :project_id => 1, :id => nil})
+      expect(test_volunteer.project_id).to eq 1
+    end
+  end
+
 #   describe '#==' do
 #     it 'checks for equality based on the name of a volunteer' do
 #       volunteer1 = Volunteer.new({:name => 'Jane', :project_id => 1, :id => nil})
@@ -54,4 +54,4 @@ require "spec_helper"
 #       expect(Volunteer.find(volunteer1.id)).to eq volunteer1
 #     end
 #   end
-# end
+end
