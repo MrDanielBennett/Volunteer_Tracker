7523268d573f4714e457e0cb6588589f1c1e68e4
MrDanielBennett <daniel.bennett1999@gmail.com>
2018-12-07 17:01:39 -0800

    add detail page

--- app.rb
+++ app.rb
@@ -9,13 +9,31 @@ DB = PG.connect({:dbname => "volunteer_tracker"})
 
 get ('/') do
   @projects = Project.all
-  erb :index
+  @volunteer = Volunteer.all
+  (erb :index)
 end
 
-post ('/') do
+post ('/new_project') do
   title = params.fetch('title')
   project = Project.new({:title => title, :id => nil})
   project.save
+  @project = Project.all
+  @volunteer = Volunteer.all
+  redirect '/'
+end
 
-  redirect "/"
+get ('/projects/:id') do
+  id = params[:id].to_i
+  @project = Project.find(id)
+  erb (:project_details)
+end
+
+patch ('/project_details/:id') do
+  title = params.fetch("title")
+  id = params[:id].to_i
+  @project = Project.find(id)
+  @project.update({:title => title, :id => nil})
+  @projects = Project.all
+  @volunteers = Volunteer.all
+  erb (:index)
 end
